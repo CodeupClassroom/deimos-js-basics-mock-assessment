8203ef5e8e0c43c21cc3cd79dc058405b6a4fb38
Roger Mayer <rmayer1984@gmail.com>
2019-10-10 15:17:42 -0500

    Rogers test

--- solutions.js
+++ solutions.js
@@ -20,3 +20,86 @@
  * and
  * concat
  */
+
+
+//#2
+
+function isTrue(x) {
+    if (x === "true") {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+//#3
+function isFalse(x){
+    if (x === "false") {
+        return true;
+    } else {
+        return false;
+    }
+}
+//#4
+//return
+function not(x){
+    var b = !x;
+    return b;
+}
+
+
+//#5 !!!!!!!!!!
+
+function addOne (x) {
+    if (!isNaN(x)) {
+        return x + 1;
+    } else {
+        return x;
+    }
+}
+
+//#6
+function isEven(x) {
+    if (x % 2 == 0){
+        return true;
+    } else  {
+        return false;
+    }
+}
+//#7
+function isIdentical(x,y) {
+    if (x === y) {
+        return true;
+    } else {
+        return false;
+    }
+}
+//#7
+function isEqual(x,y) {
+    if (x == y) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+//#8
+function or(x,y){
+    return x || y;
+}
+
+//#8
+function and(x,y){
+    return x && y;
+}
+//#9 !!!!!!!!!!!!!!!!!!!!!!
+function concat(x,y) {
+    if (isNaN(x && y)) {
+        return (x + y)
+    } else {
+        return (x.toString() + y.toString());
+    }
+}
+
+
+
