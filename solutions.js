5be073fd08447bf8906800f2ed7ed9cc3ceb49e1
Jacob Barrow <jacob.barrow@gmail.com>
2019-10-10 14:42:35 -0500

    completed deimos-js-basics-mock-assessment

--- solutions.js
+++ solutions.js
@@ -20,3 +20,48 @@
  * and
  * concat
  */
+
+
+function isTrue(input) {
+    return input === true;
+}
+
+function isFalse(input) {
+    return input === false;
+}
+
+function not(input) {
+    return !(input);
+}
+
+function addOne(input) {
+    if (typeof parseInt(input) === "number") {
+        return parseFloat(input) + 1;
+    }
+}
+
+function isEven(input) {
+   if (typeof parseInt(input) === "number") {
+           return parseFloat(input) % 2 === 0;
+   }
+}
+
+function isIdentical(x, y) {
+    return (x === y);
+}
+
+function isEqual(x, y) {
+    return (x == y);
+}
+
+function or(x, y) {
+    return (x || y);
+}
+
+function and(x, y) {
+    return (x && y);
+}
+
+function concat(x, y) {
+    return ("" + x + y);
+}
