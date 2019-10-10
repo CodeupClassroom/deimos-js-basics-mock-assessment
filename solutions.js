37737fe4f32538bdfc6f6651790177dbd00f3787
ltovar03 <55413265+laurentovar@users.noreply.github.com>
2019-10-10 15:11:52 -0500

    practice assessment typeof

--- solutions.js
+++ solutions.js
@@ -23,25 +23,25 @@
 // 1. Define a function named isTrue that takes in any input and returns true
 // if the input provided is exactly equal to true in value and data type.
 function isTrue(valueTrue) {
-    return "true" === valueTrue;
+    return typeof "true" === valueTrue;
 
 }
 // 2. Define a function named isFalse that takes in a value and returns a true
 // if and only if the provided input is equal to false in both type and value
 function isFalse(valueFalse) {
-    return "false" === valueFalse;
+    return typeof "false" === valueFalse;
 }
 
 //3. Define a function named not that takes in any input and returns
 // the boolean opposite of the provided input.
 function not(x) {
-    return !x;
+    return typeof x === !x;
 }
 
 //4. Define a function named addOne that takes in a single input.
 // If the input is a number or a numeric string, return the value plus one.
 function addOne(number) {
-    return number + 1;
+    return typeof number + 1;
 }
 
 //5. Define a function named isEven that takes in a single input.
@@ -49,11 +49,7 @@ function addOne(number) {
 // Any other input should return false for the output.
 
 function isEven(number) {
-    if (number % 2 === 0) {
-    return "true";
-    } else {
-        return "false";
-    }
+    return typeof (number % 2 === 0)
 }
 
 
@@ -61,40 +57,34 @@ function isEven(number) {
 // If each input is equal both in data type and in value, then return true.
 // If the values are not the same data type or not the same value, return false.
 function isIdentical(x,y) {
-    if (x === y) {
-        return "true";
-    } else {
-        return "false";
-    }
+    return typeof x === y
+
 }
 
 //7. Define a function named isEqual that takes in two input arguments.
 // If each argument is equal only in value, then return true. Otherwise return false.
 function isEqual(x, y) {
     if (x == y) {
-        return "true";
-    } else {
-        return "false";
-    }
+   return typeof x ==y;
 }
 
 //8. Define a function named or that takes in two input arguments.
 // The output returned should be the result of an or operation on both inputs.
 
 function or(one, two) {
-    return one || two;
+    return typeof one || two;
 }
 
 //9. Define a function named and that takes in two input arguments and returns the result
 // of a logical and operation of both inputs.
 
 function and(x, y) {
-    return x && y;
+    return typeof x && y;
 }
 
 //10. Define a function named concat that takes in two input arguments.
 // If both arguments are strings, then return the concatenated result.
 // If two numbers are provided, then return the string concatenation of each set of numerals.
 function concat(x, y) {
-   return x.concat(y);
+   return typeof x.concat(y);
 }
