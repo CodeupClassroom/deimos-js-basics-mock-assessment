3f66043ddd33314f484ea2f70860a19714ae35eb
RiverRobins <rivergrahamrobins@gmail.com>
2019-10-10 15:16:27 -0500

    commited thing

--- solutions.js
+++ solutions.js
@@ -20,3 +20,68 @@
  * and
  * concat
  */
+function isTrue(n) {
+    if (n === true) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+function isFalse(n) {
+    if (n === false) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+function not(n) {
+    return !n;
+}
+function addOne(n) {
+    if (n === Infinity) {
+        return Infinity;3
+    }
+    return parseInt(n) + 1;
+}
+function isEven(n) {
+    if (typeof n === "number" || typeof n === "string") {
+
+        if (n % 2 == 0) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    else {
+        return false;
+    }
+}
+function isIdentical(n, m) {
+    if (n === m) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+function isEqual(n, m) {
+    if (n == m) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+function or(n, m) {
+    return (n || m);
+}
+
+function and(n, m) {
+    return (n && m);
+}
+function concat(n, m) {
+    return n.toString() + m.toString();
+}
